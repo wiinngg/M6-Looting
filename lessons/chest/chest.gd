947369df38fa8502816663d3c0e7baeef5e57aa6
wiinngg <wikuang2@s.sfusd.edu>
2026-02-05 13:52:40 -0800

    M6L7 completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -50,4 +50,22 @@ func _spawn_random_item() -> void:
 	var random_angle := randf_range(0.0, 2.0 * PI)
 	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
 	var random_distance := randf_range(60.0, 120.0)
-	loot_item.position = random_direction * random_distance
+	var land_position := random_direction * random_distance
+
+	const FLIGHT_TIME := 0.4
+	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0
+
+
+	var tween := create_tween()
+	tween.set_parallel()
+	loot_item.scale = Vector2(0.25, 0.25)
+	tween.tween_property(loot_item, "scale", Vector2(1.0, 1.0), HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:x", land_position.x, FLIGHT_TIME)
+
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	var jump_height := randf_range(30.0, 80.0)
+	tween.tween_property(loot_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
